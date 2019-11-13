1e6db399de58fd52c32a7706c2cc538f4f8d4e4c
Rui Lebre <ruilebre@ua.pt>
2019-11-13 19:45:57 +0000

    replaced singleton by arrays.aslist

--- src/main/java/pt/ieeta/dicoogle/plugin/sample/SamplePluginSet.java
+++ src/main/java/pt/ieeta/dicoogle/plugin/sample/SamplePluginSet.java
@@ -17,19 +17,20 @@
  * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package pt.ieeta.dicoogle.plugin.demo.dicooglepluginsample;
+package pt.ieeta.dicoogle.plugin.sample;
 
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import org.restlet.resource.ServerResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pt.ieeta.dicoogle.plugin.demo.dicooglepluginsample.index.SampleIndexPlugin;
-import pt.ieeta.dicoogle.plugin.demo.dicooglepluginsample.query.SampleQueryPlugin;
-import pt.ieeta.dicoogle.plugin.demo.dicooglepluginsample.storage.SampleStoragePlugin;
-import pt.ieeta.dicoogle.plugin.demo.dicooglepluginsample.webservice.SampleJettyPlugin;
+import pt.ieeta.dicoogle.plugin.sample.index.SampleIndexPlugin;
+import pt.ieeta.dicoogle.plugin.sample.query.SampleQueryPlugin;
+import pt.ieeta.dicoogle.plugin.sample.storage.SampleStoragePlugin;
+import pt.ieeta.dicoogle.plugin.sample.webservice.SampleJettyPlugin;
 import pt.ua.dicoogle.sdk.*;
 import pt.ua.dicoogle.sdk.settings.ConfigurationHolder;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -59,7 +60,6 @@ public class SamplePluginSet implements PluginSet {
     public SamplePluginSet() {
         logger.info("Initializing Sample Plugin Set");
 
-        // construct all plugins here
         this.indexer = new SampleIndexPlugin(memoryDicomDB);
         this.jettyWeb = new SampleJettyPlugin();
         this.query = new SampleQueryPlugin(memoryDicomDB);
@@ -71,19 +71,12 @@ public class SamplePluginSet implements PluginSet {
 
     @Override
     public Collection<IndexerInterface> getIndexPlugins() {
-        /* Since we only have one indexer, we can return a singleton. For more than
-         * one plugin, Arrays.asList(...) is a nice alternative. If there are no
-         * plugins of this type in the set, simply return an empty set or list.
-         *
-         * This cast in the argument is needed (only in versions prior to Java 8),
-         * otherwise the return type cannot be resolved properly.
-         */
-        return Collections.singleton(this.indexer);
+        return Arrays.asList(this.indexer);
     }
 
     @Override
     public Collection<QueryInterface> getQueryPlugins() {
-        return Collections.singleton(this.query);
+        return Arrays.asList(this.query);
     }
 
     /**
@@ -103,7 +96,7 @@ public class SamplePluginSet implements PluginSet {
 
     @Override
     public Collection<JettyPluginInterface> getJettyPlugins() {
-        return Collections.singleton(this.jettyWeb);
+        return Arrays.asList(this.jettyWeb);
     }
 
     @Override
@@ -112,7 +105,7 @@ public class SamplePluginSet implements PluginSet {
 
     @Override
     public Collection<StorageInterface> getStoragePlugins() {
-        return Collections.singleton(this.storage);
+        return Arrays.asList(this.storage);
     }
 
     @Override
