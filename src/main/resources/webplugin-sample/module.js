cabd6cb54a06d6c2dac8eca7e4062f3d5071c964
Rui Lebre <ruilebre@ua.pt>
2019-11-13 20:01:24 +0000

    refactored webplugin

--- src/main/resources/webplugin-sample/module.js
+++ src/main/resources/webplugin-sample/module.js
@@ -43,45 +43,56 @@ module.exports =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports) {
 
 	'use strict';
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	        value: true
 	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	/* global Dicoogle */
 
 	var MyPlugin = function () {
-	    function MyPlugin() {
-	        _classCallCheck(this, MyPlugin);
-	    }
-	    // TODO initialize plugin here
-
-	    /** 
-	     * @param {DOMElement} parent
-	     * @param {DOMElement} slot
-	     */
-
-	    _createClass(MyPlugin, [{
-	        key: 'render',
-	        value: function render(parent, slot) {
-	            // TODO mount a new web component here
-	            var div = document.createElement('div');
-	            div.innerHTML = 'Hello, Dicoogle!';
-	            parent.appendChild(div);
+	        function MyPlugin() {
+	                _classCallCheck(this, MyPlugin);
 	        }
-	    }]);
+	        // TODO initialize plugin here
 
-	    return MyPlugin;
+
+	        /**
+	         * @param {DOMElement} parent
+	         * @param {DOMElement} slot
+	         */
+
+
+	        _createClass(MyPlugin, [{
+	                key: 'render',
+	                value: function render(parent, slot) {
+	                        // TODO mount a new web component here
+	                        var div = document.createElement('div');
+
+	                        var h1 = document.createElement('h1');
+	                        h1.innerHTML = "Hello from Dicoogle!";
+
+	                        var button = document.createElement('button');
+	                        button.innerHTML = "Click me!";
+
+	                        div.appendChild(h1);
+	                        div.appendChild(button);
+
+	                        parent.appendChild(div);
+	                }
+	        }]);
+
+	        return MyPlugin;
 	}();
 
 	exports.default = MyPlugin;
 
-/***/ }
+/***/ })
 /******/ ]);
--- src/main/resources/webplugin-sample/src/index.js
+++ src/main/resources/webplugin-sample/src/index.js
@@ -1,21 +1,30 @@
 /* global Dicoogle */
 
 export default class MyPlugin {
-    
+
     constructor() {
         // TODO initialize plugin here
     }
-    
-    /** 
+
+    /**
      * @param {DOMElement} parent
      * @param {DOMElement} slot
      */
     render(parent, slot) {
         // TODO mount a new web component here
         const div = document.createElement('div');
-        div.innerHTML = 'Hello, Dicoogle!';
+
+        const h1 = document.createElement('h1');
+        h1.innerHTML = "Hello from Dicoogle!";
+
+        const button = document.createElement('button');
+        button.innerHTML = "Click me!";
+
+        div.appendChild(h1);
+        div.appendChild(button);
+
         parent.appendChild(div);
-        
+
     }
-    
+
 }
