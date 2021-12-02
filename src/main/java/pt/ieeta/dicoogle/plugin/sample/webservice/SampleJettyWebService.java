9d37d0500801fa9b4b66a513bfcddccfef4b69bf
Rui Lebre <ruilebre@ua.pt>
2021-12-02 11:45:42 +0000

    Fixed plugin set and jetty with Dicoogle 3 api

--- src/main/java/pt/ieeta/dicoogle/plugin/sample/webservice/SampleJettyWebService.java
+++ src/main/java/pt/ieeta/dicoogle/plugin/sample/webservice/SampleJettyWebService.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2014  Universidade de Aveiro, DETI/IEETA, Bioinformatics Group - http://bioinformatics.ua.pt/
- *
+ * <p>
  * This file is part of Dicoogle/dicoogle-plugin-sample.
- *
+ * <p>
  * Dicoogle/dicoogle-plugin-sample is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Dicoogle/dicoogle-plugin-sample is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,8 +20,8 @@ package pt.ieeta.dicoogle.plugin.sample.webservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pt.ua.dicoogle.sdk.annotation.InjectPlatformProxy;
 import pt.ua.dicoogle.sdk.core.DicooglePlatformInterface;
-import pt.ua.dicoogle.sdk.core.PlatformCommunicatorInterface;
 import pt.ua.dicoogle.sdk.datastructs.SearchResult;
 import pt.ua.dicoogle.sdk.task.Task;
 
@@ -39,9 +39,10 @@ import java.util.concurrent.ExecutionException;
  * @author Luís A. Bastião Silva - <bastiao@ua.pt>
  * @author Rui Lebre - <ruilebre@ua.pt>
  */
-public class SampleJettyWebService extends HttpServlet implements PlatformCommunicatorInterface {
+public class SampleJettyWebService extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(SampleJettyWebService.class);
 
+    @InjectPlatformProxy
     private DicooglePlatformInterface platform;
 
     public SampleJettyWebService() {
@@ -84,10 +85,4 @@ public class SampleJettyWebService extends HttpServlet implements PlatformCommun
         PrintWriter out = response.getWriter();
         out.print("{\"action\":\"test\"}");
     }
-
-    @Override
-    public void setPlatformProxy(DicooglePlatformInterface core) {
-        this.platform = core;
-    }
-
 }
