21771fab164ee12d19d7c1ce3958c43d62151333
Rui Lebre <ruilebre@ua.pt>
2021-12-02 11:32:42 +0000

    Updated Dicoogle v3.0.3

--- src/main/java/pt/ieeta/dicoogle/plugin/sample/storage/SampleStoragePlugin.java
+++ src/main/java/pt/ieeta/dicoogle/plugin/sample/storage/SampleStoragePlugin.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2014  Universidade de Aveiro, DETI/IEETA, Bioinformatics Group - http://bioinformatics.ua.pt/
- *
+ * <p>
  * This file is part of Dicoogle/dicoogle-plugin-sample.
- *
+ * <p>
  * Dicoogle/dicoogle-plugin-sample is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Dicoogle/dicoogle-plugin-sample is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -61,33 +61,29 @@ public class SampleStoragePlugin implements StorageInterface {
 
     @Override
     public Iterable<StorageInputStream> at(final URI location, Object... objects) {
-        Iterable<StorageInputStream> c = new Iterable<StorageInputStream>() {
+        Iterable<StorageInputStream> c = () -> {
+            Collection c2 = new ArrayList<>();
+            StorageInputStream s = new StorageInputStream() {
 
-            @Override
-            public Iterator<StorageInputStream> iterator() {
-                Collection c2 = new ArrayList<>();
-                StorageInputStream s = new StorageInputStream() {
+                @Override
+                public URI getURI() {
+                    return location;
+                }
 
-                    @Override
-                    public URI getURI() {
-                        return location;
-                    }
+                @Override
+                public InputStream getInputStream() {
+                    ByteArrayOutputStream bos = mem.get(location.toString());
+                    ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
+                    return bin;
+                }
 
-                    @Override
-                    public InputStream getInputStream() throws IOException {
-                        ByteArrayOutputStream bos = mem.get(location.toString());
-                        ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
-                        return bin;
-                    }
-
-                    @Override
-                    public long getSize() throws IOException {
-                        return mem.get(location.toString()).size();
-                    }
-                };
-                c2.add(s);
-                return c2.iterator();
-            }
+                @Override
+                public long getSize() {
+                    return mem.get(location.toString()).size();
+                }
+            };
+            c2.add(s);
+            return c2.iterator();
         };
         return c;
     }
